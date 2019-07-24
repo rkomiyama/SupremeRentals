e0f7d0617b6e575a005a8d3a43f55bbcb0045af3
rkomiyama <ryukomiyama@gmail.com>
2019-07-24 00:52:12 -0400

    Added dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-app id="inspire">
+    <NavBar />
+    <SiteContent />
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import NavBar from './components/NavBar';
+import SiteContent from './components/SiteContent';
+import Footer from './components/Footer';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    NavBar,
+    SiteContent,
+    Footer
   },
-  data: () => ({
-    //
-  }),
-};
+  props: {
+    source: String,
+  },
+  created () {
+    this.$vuetify.theme.dark = true
+  },
+}
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-layout>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Supreme Rentals</v-toolbar-title>
+    </v-app-bar>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
