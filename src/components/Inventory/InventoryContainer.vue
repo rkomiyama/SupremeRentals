66a4ca0fa4d66c64d6dfd269d7145f78f3fb44f7
rkomiyama <ryukomiyama@gmail.com>
2019-07-28 21:06:04 -0400

    Sets up vuex store

--- src/components/Inventory/InventoryContainer.vue
+++ src/components/Inventory/InventoryContainer.vue
@@ -1,8 +1,6 @@
 <template>
   <v-container fluid>
-    <v-layout
-      wrap
-    >
+    <v-layout wrap>
       <InventoryCard
         v-for="(car, i) in inventory"
         :key="i"
@@ -13,24 +11,15 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import InventoryCard from './InventoryCard'
 
 export default {
   name: 'InventoryContainer',
-  data () {
-    return {
-      inventory: []
-    }
-  },
-  created () {
-    for (let i = 0; i < 20; i++ ) {
-      this.inventory.push({
-        rate: 125,
-        year: 2016,
-        make: 'Mazda',
-        model: 'MX-5'
-      })
-    }
+  computed: {
+    ...mapState({
+      inventory: state => state.inventory
+    })
   },
   components: {
     InventoryCard
